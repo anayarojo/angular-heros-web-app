705978d1bb98229a2d6a6fbf10d26f861bf49388
Raul Anaya <raulmartin_04@hotmail.com>
2023-11-08 08:43:42 -0700

    Fix email typo

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -14,7 +14,7 @@ export class LoginPageComponent {
   constructor(private router: Router, private authService: AuthService) {}
 
   public login(): void {
-    this.authService.login('john.due@email.com', 'secret')
+    this.authService.login('john.doe@email.com', 'secret')
       .subscribe(logged => {
         if (logged) {
           this.router.navigate(['/']);
